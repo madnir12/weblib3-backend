01660576f686ddeae1b15eea3ab94a37d4295450
madnir12 <anfz12001@gmail.com>
2023-03-10 14:25:18 +0500

    backend initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express"),
+  cors = require("cors"),
+  mongoose = require("mongoose"), // <-- fix typo here
+  usersRouter = require("./routes/users");
+require("dotenv").config();
+
+const app = express(),
+  port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/users",usersRouter);
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true,  useUnifiedTopology: true })      
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
