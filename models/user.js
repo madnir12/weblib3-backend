282d747f23821ffe94dd469754581a9f7389c219
madnir12 <anfz12001@gmail.com>
2023-03-18 18:21:36 +0500

    users removed

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose"), // getting mongoose
-Schema = mongoose.Schema, // getting schema 
-userSchema = new Schema({
-  firstName: {type: String,required: true,minlength: 3},
-  lastName: {type: String,required: true,minlength: 3}
-},{timestamps: true}), // creating a new schema
-User = mongoose.model("User",userSchema); // creating model
-module.exports = User;
